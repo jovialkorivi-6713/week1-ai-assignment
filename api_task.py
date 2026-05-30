27669321c6e388042750bd266812eb9a22a295ed
jovialkorivi-6713 <jovialkorivi@gmail.com>
2026-05-30 16:16:17 +0530

    Added Python project files

---
+++ api_task.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://catfact.ninja/fact"
+
+response = requests.get(url)
+
+data = response.json()
+
+print("🐱 Random Cat Fact")
+print("------------------")
+print(data["fact"])
---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+
+def greet():
+    print("Welcome to Week 1 AI Assignment!")
+
+def add_numbers(a, b):
+    return a + b
+
+greet()
+
+result = add_numbers(10, 20)
+print("Sum:", result)
